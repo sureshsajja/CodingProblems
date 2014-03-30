a48d2e2ab0088a42d4629e332263050ff2371297
Suresh <mail@sureshsajja.com>
2014-03-31 00:45:37 +0530

    multiplicative inverse of 3 modulo (prime number)

---
+++ src/com/hackerearth/ModInverseCalculator.java
@@ -0,0 +1,18 @@
+package com.hackerearth;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public class ModInverseCalculator {
+
+    public static void main(String[] args) {
+        BigInteger largePrime = BigInteger.probablePrime(31, new Random());
+        System.out.println("large prime generated " + largePrime.longValue());
+        BigInteger inverse = BigInteger.valueOf(3L).modInverse(largePrime);
+        System.out.println("multiplicative inverse of 3 modulo (prime number) = " + inverse);
+
+        System.out.println("verification : (inverse*3) % (prime number) = " + (inverse.longValue() * 3L) % largePrime.longValue());
+
+
+    }
+}
