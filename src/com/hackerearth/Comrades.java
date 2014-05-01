7f40369a10786508991150a801a169b195a25e59
Suresh Sajja <suresh.sajja@autodesk.com>
2014-05-01 14:02:08 +0530

    comrades

--- src/com/hackerearth/Comrades.java
+++ src/com/hackerearth/Comrades.java
@@ -6,33 +6,19 @@ import java.util.Stack;
 
 /**
  * It’s the year 2552, the Humans just won a war against a very powerful alien race that had invaded our solar system. The Human army is in celebration mode!
- * <p/>
  * The army has n soldiers. The soldiers are numbers from 1 to n. The army has a superiority hierarchy. Every soldier has one immediate superior. The superior of a superior of a soldier is also a superior to that soldier. So, a soldier may have one or more superiors but only one immediate superior.
- * <p/>
  * Every soldier has to congratulate every other soldier. For a pair of soldiers if one of them is the superior of the other, they will shake hands. Otherwise, they will bump their fists.
- * <p/>
  * You are given the list of immediate superior of all soldiers. Your job is to tell how many handshakes and fist bumps will be there.
- * <p/>
  * NOTE: Among all soldiers there is one soldier who does not have any superior. He is the commander of the whole army.
- * <p/>
  * Input:
- * <p/>
  * The first line of the input contains t, the number of test cases.
- * <p/>
  * The first line of each test case contains n, the number of soldiers. The next line contains n space separated integers. The ith integer represents the immediate superior of the ith soldier.
- * <p/>
  * The immediate superior of the commander of the army will be '0'.
- * <p/>
  * Output:
- * <p/>
  * Output two space separated integers, the number of handshakes and the number of fist bumps.
- * <p/>
  * Constraints:
- * <p/>
  * 1<=t<=10
- * <p/>
  * 1<=n<=100000
- * <p/>
  * It is guaranteed that the commander of the army is a superior of all soldiers.
  */
 public class Comrades {
