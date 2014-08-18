5762f54a18fd219a42c157b0ad30b1a1959d5df9
Suresh Sajja <suresh.sajja@autodesk.com>
2014-08-18 14:59:58 +0530

    Necklace

--- src/com/hackerrank/Necklace.java
+++ src/com/hackerrank/Necklace.java
@@ -1,11 +1,14 @@
 package com.hackerrank;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 //TODO optimise this
 public class Necklace
 {
-    private static int MAX = 10000;
+    private static int MAX = 1000;
     private static long[][] matrix = new long[MAX + 1][MAX + 1];
     private static int MOD = 1000000000 + 7;
 
@@ -13,21 +16,29 @@ public class Necklace
     {
 
         Scanner scanner = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
         int T = scanner.nextInt();
         for (int t = 0; t < T; t++) {
             int i = scanner.nextInt();
-            System.out.println(count(i));
+            pw.println(count(i));
         }
 
+        pw.close();
+        scanner.close();
     }
 
     private static long count(int i)
     {
         long result = 1;
         for (int n = i, k = 1; n >= k; n -= 2, k++) {
-            if (matrix[n][k] == 0)
-                calculateValue(n, k);
-            result = (result + matrix[n][k]) % MOD;
+            int r = k;
+            if (k > n / 2) {
+                r = n - k;
+            }
+            if (matrix[n][r] == 0) {
+                calculateValue(n, r);
+            }
+            result = (result + matrix[n][r]) % MOD;
         }
         return result;
     }
@@ -35,8 +46,9 @@ public class Necklace
     private static long calculateValue(int n, int k)
     {
 
-        if (matrix[n][k] != 0)
+        if (matrix[n][k] != 0) {
             return matrix[n][k];
+        }
 
         if (k == 0 || n == 0 || n == k) {
             matrix[n][k] = 1;
