8c1e47969b2bb48d1188e094db1e9b33cf8fb997
Suresh <mail@sureshsajja.com>
2014-09-15 19:45:54 +0530

    Lenskart

---
+++ src/com/hackerearth/lenskart/AndiAndBob.java
@@ -0,0 +1,72 @@
+package com.hackerearth.lenskart;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+//TODO optimise
+public class AndiAndBob
+{
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        int T = scanner.nextInt();
+        for (int t = 0; t < T; t++) {
+            int N = scanner.nextInt();
+            int[] array = new int[N];
+            for (int n = 0; n < N; n++) {
+                int i = scanner.nextInt();
+                if (i % 2 == 0) {
+                    array[n] = 0;
+                } else {
+                    array[n] = 1;
+                }
+            }
+
+            int[] evenOdd = new int[2];
+
+
+            subArrayXor(array, 0, N - 1, evenOdd);
+
+            if (evenOdd[0] > evenOdd[1]) {
+                pw.println("ANDI");
+            } else {
+                pw.println("BOB");
+            }
+        }
+
+        scanner.close();
+        pw.close();
+    }
+
+    private static void subArrayXor(int[] array, int l, int h, int[] evenOdd)
+    {
+        if (l == h) {
+            evenOdd[array[l]]++;
+            return;
+        }
+
+        int m = (l + h) / 2;
+
+        subArrayXor(array, l, m, evenOdd);
+        subArrayXor(array, m + 1, h, evenOdd);
+        subCrossingXor(array, l, m, h, evenOdd);
+    }
+
+    private static void subCrossingXor(int arr[], int l, int m, int h, int[] evenOdd)
+    {
+
+        for (int k = m; k >= l; k--) {
+            int intermediate = arr[k];
+            for (int i = m + 1; i <= h; i++) {
+                intermediate ^= arr[m];
+                evenOdd[intermediate]++;
+            }
+        }
+    }
+
+}
