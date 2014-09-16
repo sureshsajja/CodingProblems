64dd8968a85c5ced1ababea2e82400ca1abe664f
Suresh Sajja <suresh.sajja@autodesk.com>
2014-09-16 11:22:23 +0530

    RMQ

---
+++ src/com/coderevisited/rmq/SegmentTreeRMQ.java
@@ -0,0 +1,81 @@
+package com.coderevisited.rmq;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import com.coderevisited.utils.MathUtils;
+
+public class SegmentTreeRMQ
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        int N = scanner.nextInt();
+        int[] array = new int[N];
+        for (int n = 0; n < N; n++) {
+            array[n] = scanner.nextInt();
+        }
+
+        int logN = (int) MathUtils.log(N, 2);
+        int pow = (int) Math.pow(2, logN + 1);
+        int K = 2 * pow;
+        int[] matrix = new int[K];
+
+        init(0, 0, N - 1, matrix, array);
+
+        int T = scanner.nextInt();
+        for (int t = 0; t < T; t++) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            pw.println(query(0, 0, N - 1, matrix, array, A, B));
+        }
+
+        scanner.close();
+        pw.close();
+    }
+
+    private static int query(int index, int b, int e, int[] matrix, int[] array, int A, int B)
+    {
+        if (A > e || B < b) {
+            return -1;
+        }
+
+        if (b >= A && e <= B) {
+            return matrix[index];
+        }
+
+        int p1 = query(2 * index + 1, b, (b + e) / 2, matrix, array, A, B);
+        int p2 = query(2 * index + 2, (b + e) / 2 + 1, e, matrix, array, A, B);
+
+        if (p1 == -1) {
+            return p2;
+        }
+        if (p2 == -1) {
+            return p1;
+        }
+        if (array[p1] <= array[p2]) {
+            return p1;
+        }
+        return p2;
+    }
+
+    private static void init(int index, int i, int j, int[] matrix, int[] array)
+    {
+        if (i == j) {
+            matrix[index] = i;
+        } else {
+            init(2 * index + 1, i, (i + j) / 2, matrix, array);
+            init(2 * index + 2, (i + j) / 2 + 1, j, matrix, array);
+
+            if (array[matrix[2 * index + 1]] < array[matrix[2 * index + 2]]) {
+                matrix[index] = matrix[2 * index + 1];
+            } else {
+                matrix[index] = matrix[2 * index + 2];
+            }
+        }
+    }
+}
