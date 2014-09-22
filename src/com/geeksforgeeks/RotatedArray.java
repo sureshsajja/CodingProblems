c11aadc2c4d8de45c763e0f74483d570b02a95e9
Suresh <mail@sureshsajja.com>
2014-09-23 00:20:30 +0530

    Rotated Array

--- src/com/geeksforgeeks/RotatedArray.java
+++ src/com/geeksforgeeks/RotatedArray.java
@@ -4,7 +4,22 @@ public class RotatedArray
 {
     public static void main(String[] args)
     {
-        int[] array = new int[]{7, 8, 9, 10, 1, 2, 3, 4, 5, 6};
+        int[] array = new int[]{6, 7, 8, 9, 10, 11, 1, 2, 3, 4};
+        //int[] array = new int[]{5, 6, 1, 2, 3, 4};
+        // int[] array = new int[]{1, 0, 2, 3, 4};
+        //int[] array = new int[]{2, 1};
+        // int[] array = new int[]{1};
+        //int[] array = new int[]{1, 2};
+        //int[] array = new int[]{5, 6, 7, 1, 2, 3, 4};
+        // int[] array = new int[]{1, 2, 3, 4, 5, 6, 7};
+        // int[] array = new int[]{2, 3, 4, 5, 6, 7, 8, 1};
+        //  int[] array = new int[]{3, 4, 5, 1, 2};
+        // int[] array = new int[]{2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 1, 2};
+        //int[] array = new int[]{2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2};
+        // int[] array = new int[]{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1};
+        // int[] array = new int[]{3, 3, 3, 4, 4, 4, 4, 5, 3, 3};
+        // int[] array = new int[]{1, 1, 2, 2, 3};
+        //  int[] array = new int[]{1, 1, 0, 1};
 
         int min = findMin(array, 0, array.length - 1);
         int a = -1;
@@ -22,9 +37,18 @@ public class RotatedArray
     {
         if (low < high) {
             int mid = low + (high - low) / 2;
-            if (mid > 0 && array[mid] < array[mid - 1]) {
+
+            if (mid > low && array[mid] < array[mid - 1]) {
                 return mid;
-            } else {
+            }
+            if (mid < high && array[mid + 1] < array[mid])
+                return mid + 1;
+
+            if (array[high] > array[mid]) {
+                return findMin(array, low, mid - 1);
+            } else if (array[high] < array[mid])
+                return findMin(array, mid + 1, high);
+            else {
                 int a = findMin(array, mid + 1, high);
                 int b = findMin(array, low, mid - 1);
                 if (array[a] < array[b])
