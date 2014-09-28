68ab5d7e6f27585424380aa566460fabd501a336
Suresh <mail@sureshsajja.com>
2014-09-28 12:01:36 +0530

    Prime Numbers

--- src/com/coderevisited/utils/SieveOfEratosthenes.java
+++ src/com/coderevisited/utils/SieveOfEratosthenes.java
@@ -1,91 +1,39 @@
 package com.coderevisited.utils;
 
-/**
- * Implementation of Sieve Of Eratosthenes.. Generating prime numbers up to 1 billion using wheel_2357
- */
+import java.util.BitSet;
+
 public class SieveOfEratosthenes
 {
-
-    private static final int MAX = 1000000000;
-
+    private static int MAX = 1000000000;
+    private static BitSet bitSet = new BitSet(MAX);
 
     public static void main(String[] args)
     {
 
         long start = System.nanoTime();
 
-        SieveOfEratosthenes sieveOfEratosthenes = new SieveOfEratosthenes();
-        sieveOfEratosthenes.sieve();
+        sieve();
 
         System.out.println((double) (System.nanoTime() - start) / 1000000000.0);
 
     }
 
-    private void sieve()
+    private static void sieve()
     {
-
-        BitVector vector = new BitVector(MAX);
-        int[] seq = new int[]{2, 4, 2, 4, 6, 2, 6, 4,
-                2, 4, 6, 6, 2, 6, 4, 2,
-                6, 4, 6, 8, 4, 2, 4, 2,
-                4, 8, 6, 4, 6, 2, 4, 6,
-                2, 6, 6, 4, 2, 4, 6, 2,
-                6, 4, 2, 4, 2, 10, 2, 10};
-        int index = 0;
-        for (int i = 11; i < Math.sqrt(MAX); i += seq[index++]) {
-            if (!vector.isSet(i)) {
-                int index2 = index;
-                for (int j = i * i; j < MAX; j = j + seq[index2++] * i) {
-                    vector.setBit(j);
-                    if (index2 == 48) {
-                        index2 = 0;
-                    }
+        for (int i = 3; i < Math.sqrt(MAX); i += 2) {
+            if (!bitSet.get(i)) {
+                for (int j = i * i; j < MAX; j += i) {
+                    bitSet.set(j);
                 }
-
             }
-            if (index == 48) {
-                index = 0;
-            }
-
         }
 
-        int count = 4;
-        index = 0;
-        for (int i = 11; i < MAX; i += seq[index++]) {
-            if (!vector.isSet(i)) {
+        int count = 1;
+        for (int i = 3; i < MAX; i += 2) {
+            if (!bitSet.get(i)) {
                 count++;
-            }
-            if (index == 48) {
-                index = 0;
             }
         }
         System.out.println(count);
     }
-
-    private class BitVector
-    {
-
-        private int[] bitArray;
-
-        public BitVector(long MAX)
-        {
-            bitArray = new int[(int) (MAX >> 6) + 1];
-        }
-
-        public void setBit(long i)
-        {
-            int index = (int) (i >> 6);
-            int bit = (int) (i >> 1) & 31;
-            bitArray[index] = (bitArray[index]) | (1 << bit);
-        }
-
-        public boolean isSet(long i)
-        {
-            int index = (int) (i >> 6);
-            int bit = (int) (i >> 1) & 31;
-            return ((bitArray[index]) & (1 << bit)) != 0;
-        }
-    }
-
-
 }
---
+++ src/com/coderevisited/utils/SieveOfEratosthenesWheel2357.java
@@ -0,0 +1,91 @@
+package com.coderevisited.utils;
+
+/**
+ * Implementation of Sieve Of Eratosthenes.. Generating prime numbers up to 1 billion using wheel_2357
+ */
+public class SieveOfEratosthenesWheel2357
+{
+
+    private static final int MAX = 1000000000;
+
+
+    public static void main(String[] args)
+    {
+
+        long start = System.nanoTime();
+
+        SieveOfEratosthenesWheel2357 sieveOfEratosthenesWheel2357 = new SieveOfEratosthenesWheel2357();
+        sieveOfEratosthenesWheel2357.sieve();
+
+        System.out.println((double) (System.nanoTime() - start) / 1000000000.0);
+
+    }
+
+    private void sieve()
+    {
+
+        BitVector vector = new BitVector(MAX);
+        int[] seq = new int[]{2, 4, 2, 4, 6, 2, 6, 4,
+                2, 4, 6, 6, 2, 6, 4, 2,
+                6, 4, 6, 8, 4, 2, 4, 2,
+                4, 8, 6, 4, 6, 2, 4, 6,
+                2, 6, 6, 4, 2, 4, 6, 2,
+                6, 4, 2, 4, 2, 10, 2, 10};
+        int index = 0;
+        for (int i = 11; i < Math.sqrt(MAX); i += seq[index++]) {
+            if (!vector.isSet(i)) {
+                int index2 = index;
+                for (int j = i * i; j < MAX; j = j + seq[index2++] * i) {
+                    vector.setBit(j);
+                    if (index2 == 48) {
+                        index2 = 0;
+                    }
+                }
+
+            }
+            if (index == 48) {
+                index = 0;
+            }
+
+        }
+
+        int count = 4;
+        index = 0;
+        for (int i = 11; i < MAX; i += seq[index++]) {
+            if (!vector.isSet(i)) {
+                count++;
+            }
+            if (index == 48) {
+                index = 0;
+            }
+        }
+        System.out.println(count);
+    }
+
+    private class BitVector
+    {
+
+        private int[] bitArray;
+
+        public BitVector(long MAX)
+        {
+            bitArray = new int[(int) (MAX >> 6) + 1];
+        }
+
+        public void setBit(long i)
+        {
+            int index = (int) (i >> 6);
+            int bit = (int) (i >> 1) & 31;
+            bitArray[index] = (bitArray[index]) | (1 << bit);
+        }
+
+        public boolean isSet(long i)
+        {
+            int index = (int) (i >> 6);
+            int bit = (int) (i >> 1) & 31;
+            return ((bitArray[index]) & (1 << bit)) != 0;
+        }
+    }
+
+
+}
