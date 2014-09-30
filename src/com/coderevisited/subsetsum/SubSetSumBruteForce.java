046760c8d7ebf2a6bcc5a44f680bca5fe5cab41b
Suresh Sajja <suresh.sajja@autodesk.com>
2014-09-30 16:26:59 +0530

    SubsetSum

--- src/com/coderevisited/subsetsum/SubSetSumBruteForce.java
+++ src/com/coderevisited/subsetsum/SubSetSumBruteForce.java
@@ -1,25 +1,35 @@
 package com.coderevisited.subsetsum;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
 public class SubSetSumBruteForce
 {
     public static void main(String[] args)
     {
-        int[] set = new int[]{7, 10, 11, 12, 4, 5, 6, 3, 2, 1};
-        int sum = 8;
-        printSubsets(0, set, sum, new int[set.length], 0, 0);
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        int[] set = new int[]{-624, -915, 397, 452, 870, -658, 362, 590, 952, 44, 645, 54, -326, 376, 856, -983, 170,
+                -874, 503, 915, -847, -982, 999, 475, -880, 756, 183, -266, 423, -745, 813};
+        int sum = 0;
+        long start = System.nanoTime();
+        printSubsets(0, set, sum, new int[set.length], 0, 0, pw);
+        pw.println((double) (System.nanoTime() - start) / 1000000000.0);
+        pw.close();
     }
 
-    private static void printSubsets(int start, int[] set, int sum, int[] result, int currentSum, int index)
+    private static void printSubsets(int start, int[] set, int sum, int[] result, int currentSum, int index,
+                                     PrintWriter pw)
     {
         if (currentSum == sum && index > 0) {
             for (int i = 0; i < index; i++) {
-                System.out.print(result[i] + " ");
+                pw.print(result[i] + " ");
             }
-            System.out.println();
+            pw.println();
         } else {
             for (int i = start; i < set.length; i++) {
                 result[index] = set[i];
-                printSubsets(i + 1, set, sum, result, currentSum + set[i], index + 1);
+                printSubsets(i + 1, set, sum, result, currentSum + set[i], index + 1, pw);
             }
         }
     }
