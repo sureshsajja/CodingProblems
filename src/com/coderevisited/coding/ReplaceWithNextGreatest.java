8f680640ba922c6c1a325b3b4017d68f434e6e98
Suresh <mail@sureshsajja.com>
2014-10-02 23:47:12 +0530

    Replace With next greatest

--- src/com/coderevisited/coding/ReplaceWithNextGreatest.java
+++ src/com/coderevisited/coding/ReplaceWithNextGreatest.java
@@ -1,25 +1,44 @@
 package com.coderevisited.coding;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
 public class ReplaceWithNextGreatest
 {
     public static void main(String[] args)
     {
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
         int[] array = new int[]{16, 6, 9, 10, 11, 17, 4, 3, 9, 8, 5, 2, 14};
         replaceWithNextGreatest(array);
+        printArray(array, pw);
 
-        for (int i : array) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
+        array = new int[]{-2, -7, -8};
+        replaceWithNextGreatest(array);
+        printArray(array, pw);
+
+
+        pw.close();
     }
 
     private static void replaceWithNextGreatest(int[] array)
     {
-        int max = -1;
+        int max = Integer.MIN_VALUE;
         for (int i = array.length - 1; i >= 0; i--) {
             int current = array[i];
             array[i] = max;
             max = max > current ? max : current;
         }
+        array[array.length - 1] = -1;
+    }
+
+    private static void printArray(int[] array, PrintWriter pw)
+    {
+        for (int i : array) {
+            pw.print(i + " ");
+        }
+        pw.println();
+
     }
 }
