c049581b1f000b5ac80cab012dc0b4c759ba7b66
Suresh <mail@sureshsajja.com>
2014-10-02 14:37:00 +0530

    Duplicate count

--- src/com/coderevisited/searching/DuplicateCount.java
+++ src/com/coderevisited/searching/DuplicateCount.java
@@ -4,10 +4,10 @@ public class DuplicateCount
 {
     public static void main(String[] args)
     {
-        int[] array = new int[]{2, 2, 2, 2, 2, 2, 2, 2};
+        int[] array = new int[]{0, 0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 4};
 
-        int min = leastIndexBinarySearch(array, 0, array.length - 1, 3);
-        int max = maxIndexBinarySearch(array, 0, array.length - 1, 3);
+        int min = leastIndexBinarySearch(array, 0, array.length - 1, 1);
+        int max = maxIndexBinarySearch(array, 0, array.length - 1, 1);
         System.out.println("min:" + min);
         System.out.println("max:" + max);
         if (min != -1 && max != -1)
@@ -23,12 +23,11 @@ public class DuplicateCount
             int mid = low + (high - low) / 2;
             if ((array[mid] == key) && (mid == 0 || array[mid - 1] < key)) {
                 return mid;
-            } else if (array[mid] == key) {
-                high = mid;
-            } else if (array[mid] > key) {
-                high = mid - 1;
-            } else
+            }
+            if (array[mid] < key) {
                 low = mid + 1;
+            } else
+                high = mid - 1;
         }
         return -1;
     }
