a496307c4eff6616ae888db666250ece2fc49e7a
Suresh Sajja <suresh.sajja@autodesk.com>
2014-10-16 18:08:56 +0530

    Singleton

---
+++ src/com/coderevisited/singleton/SingletonExpected.java
@@ -0,0 +1,16 @@
+package com.coderevisited.singleton;
+
+public class SingletonExpected
+{
+    private String parameter;
+
+    public SingletonExpected(String parameter)
+    {
+        this.parameter = parameter;
+    }
+
+    public String getParameter()
+    {
+        return parameter;
+    }
+}
