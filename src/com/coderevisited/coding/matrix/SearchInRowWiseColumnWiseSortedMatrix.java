b669da4d5b5d567c52dd1265fb954407e3f40d3a
Suresh <mail@sureshsajja.com>
2014-10-25 00:48:33 +0530

    Search in Row wise Column wise sorted matrix

---
+++ src/com/coderevisited/coding/matrix/SearchInRowWiseColumnWiseSortedMatrix.java
@@ -0,0 +1,36 @@
+package com.coderevisited.coding.matrix;
+
+public class SearchInRowWiseColumnWiseSortedMatrix
+{
+    public static void main(String[] args)
+    {
+        int[][] matrix = new int[][]{{10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {27, 29, 37, 48},
+                {32, 33, 39, 50}};
+
+        int[] result = findElement(matrix, 4, 30);
+        if (result != null) {
+            System.out.println("position of the key : (" + result[0] + "," + result[1] + ")");
+        } else {
+            System.out.println("key not found");
+        }
+    }
+
+    private static int[] findElement(int[][] matrix, int n, int key)
+    {
+        int i = 0, j = n - 1;
+        while (j >= 0 && i < n) {
+            if (matrix[i][j] == key) {
+                return new int[]{i, j};
+            } else if (matrix[i][j] > key) {
+                j--;
+            } else
+                i++;
+        }
+
+        return null;
+    }
+
+}
+
