52271161fdea9f7d89410cbfc948b5d578fa0ec2
Suresh <mail@sureshsajja.com>
2014-10-25 04:31:40 +0530

    Number of Islands in 2D boolean matrix

---
+++ src/com/coderevisited/coding/matrix/NumOfIslands.java
@@ -0,0 +1,51 @@
+package com.coderevisited.coding.matrix;
+
+public class NumOfIslands
+{
+
+    private static final int[] rowNeighbours = new int[]{-1, -1, -1, 0, 0, 1, 1, 1};
+    private static final int[] columnNeighbours = new int[]{-1, 0, 1, -1, 1, -1, 0, 1};
+
+    public static void main(String[] args)
+    {
+        int N = 5, M = 5;
+        int[][] matrix = new int[][]{{1, 1, 0, 0, 0},
+                {0, 1, 0, 0, 1},
+                {1, 0, 0, 1, 1},
+                {0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 1}};
+        int result = CountIslands(matrix, N, M);
+        System.out.println("Total number of islands " + result);
+    }
+
+    private static int CountIslands(int[][] matrix, int N, int M)
+    {
+        boolean[][] visited = new boolean[N][M];
+
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (matrix[i][j] == 1 && !visited[i][j]) {
+                    dfs(matrix, i, j, visited, N, M);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private static void dfs(int[][] matrix, int i, int j, boolean[][] visited, int N, int M)
+    {
+        visited[i][j] = true;
+        for (int k = 0; k < 8; k++) {
+            int row = i + rowNeighbours[k];
+            int column = j + columnNeighbours[k];
+            if (row >= 0 && row < N && column >= 0 && column < M && !visited[row][column] && matrix[row][column] == 1) {
+                dfs(matrix, row, column, visited, N, M);
+            }
+        }
+    }
+
+
+}
