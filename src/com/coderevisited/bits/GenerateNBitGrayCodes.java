b5aa0b8cebf657ff5b03ce66698c46010eebb580
Suresh <mail@sureshsajja.com>
2014-10-25 12:50:10 +0530

    Generate N-Bit Gray Codes

---
+++ src/com/coderevisited/bits/GenerateNBitGrayCodes.java
@@ -0,0 +1,36 @@
+package com.coderevisited.bits;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateNBitGrayCodes
+{
+    public static void main(String[] args)
+    {
+        List<String> list = generateGrayCode(5);
+        for (String code : list) {
+            System.out.println(code);
+        }
+    }
+
+    private static List<String> generateGrayCode(int n)
+    {
+        if (n == 1) {
+            List<String> list = new ArrayList<>();
+            list.add("0");
+            list.add("1");
+            return list;
+        }
+        List<String> prev = generateGrayCode(n - 1);
+        List<String> list = new ArrayList<>();
+        for (String str : prev) {
+            list.add("0" + str);
+        }
+
+        for (int i = prev.size() - 1; i >= 0; i--) {
+            list.add("1" + prev.get(i));
+        }
+
+        return list;
+    }
+}
