8d22d274a0418b70730b077b6b5cbe6cd12f1576
Suresh <mail@sureshsajja.com>
2014-10-25 12:23:17 +0530

    Rotations of EachOther

---
+++ src/com/coderevisited/strings/RotationsOfEachOther.java
@@ -0,0 +1,31 @@
+package com.coderevisited.strings;
+
+public class RotationsOfEachOther
+{
+    public static void main(String[] args)
+    {
+        String s1 = "ABCD";
+        String s2 = "CDAB";
+        if (areRotations(s1, s2)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        s1 = "ABCD";
+        s2 = "ACBD";
+        if (areRotations(s1, s2)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    private static boolean areRotations(String s1, String s2)
+    {
+
+        String temp = s1 + s1;
+
+        return temp.contains(s2);
+    }
+}
