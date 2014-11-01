f23410869f3ac766545e7e507821490efcf8f3f3
Suresh <mail@sureshsajja.com>
2014-11-02 00:11:44 +0530

    Array Rotation Block swap

--- src/com/coderevisited/arrays/rotation/ArrayRotationBlockSwap.java
+++ src/com/coderevisited/arrays/rotation/ArrayRotationBlockSwap.java
@@ -1,5 +1,20 @@
 package com.coderevisited.arrays.rotation;
 
+/**
+ * Initialize A = arr[0..d-1] and B = arr[d..n-1]
+ * 1) Do following until size of A is equal to size of B
+ * <p/>
+ * a)  If A is shorter, divide B into Bl and Br such that Br is of same
+ * length as A. Swap A and Br to change ABlBr into BrBlA. Now A
+ * is at its final place, so recur on pieces of B.
+ * <p/>
+ * b)  If A is longer, divide A into Al and Ar such that Al is of same
+ * length as B Swap Al and B to change AlArB into BArAl. Now B
+ * is at its final place, so recur on pieces of A.
+ * <p/>
+ * 2)  Finally when A and B are of equal size, block swap them.
+ */
+
 public class ArrayRotationBlockSwap
 {
     public static void main(String[] args)
