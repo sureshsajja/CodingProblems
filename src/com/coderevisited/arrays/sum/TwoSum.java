a45bf71e92253272d4834d6aee19489b0feac385
Suresh <mail@sureshsajja.com>
2014-11-02 02:19:08 +0530

    Two Sum

--- src/com/coderevisited/arrays/sum/TwoSum.java
+++ src/com/coderevisited/arrays/sum/TwoSum.java
@@ -1,4 +1,6 @@
-package com.coderevisited.coding.sum;
+package com.coderevisited.arrays.sum;
+
+import com.coderevisited.searching.BinarySearch;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -6,8 +8,28 @@ import java.util.Set;
 
 /**
  * 2-SUM problem
+ * Check if any pair exists :
+ * 1. Sort the array
+ * 2. Use two pointers, low points to first element, high points to last elements
+ * 3. if(array[low]+array[high] == SUM) return low, high
+ * 4. if(array[low]+array[high] > SUM) decrement high;
+ * 5. if(array[low]+array[high] < SUM) increment low;
+ * 6. Repeat 3,4,5 until low < high
+ * <p/>
+ * <p/>
+ * <p/>
+ * Find all pairs :
+ * Approach 1:
+ * ===========
+ * 1. Sort the array
+ * 2. For each element i, using binary search find if there exists sum - i. return pair
+ * <p/>
+ * Approach 2:
+ * ===========
+ * For each element 'i' in the array, construct HashSet progressively.
+ * At each step, check if there is 'sum - i' exists in the hash set. return i, sum - i.
  */
-public class Pair
+public class TwoSum
 {
 
     public static void main(String[] args)
@@ -16,8 +38,7 @@ public class Pair
         //distinct integers
         int[] array = new int[]{1, 4, 45, 6, 10, -8};
         int[] result = findTwoSum(array, 16);
-        if (result != null)
-        {
+        if (result != null) {
             System.out.println("values that make the given sum : " + result[0] + " and " + result[1]);
         }
 
@@ -30,10 +51,8 @@ public class Pair
     private static void findAllPairsByHashing(int[] array, int sum)
     {
         Set<Integer> set = new HashSet<>(array.length);
-        for (int i : array)
-        {
-            if (set.contains(sum - i))
-            {
+        for (int i : array) {
+            if (set.contains(sum - i)) {
                 System.out.println("values that make the given sum : " + i + " and " + (sum - i));
             }
             set.add(i);
@@ -47,16 +66,12 @@ public class Pair
 
         int low = 0, high = array.length - 1;
 
-        while (low < high)
-        {
-            if (array[low] + array[high] == sum)
-            {
+        while (low < high) {
+            if (array[low] + array[high] == sum) {
                 return new int[]{array[low], array[high]};
-            } else if (array[low] + array[high] > sum)
-            {
+            } else if (array[low] + array[high] > sum) {
                 high--;
-            } else if (array[low] + array[high] < sum)
-            {
+            } else if (array[low] + array[high] < sum) {
                 low++;
             }
         }
@@ -67,12 +82,10 @@ public class Pair
     public static void findAllPairs(int[] array, int sum)
     {
         Arrays.sort(array);
-        for (int i = 0; i < array.length - 1; i++)
-        {
+        for (int i = 0; i < array.length - 1; i++) {
             int toFind = sum - array[i];
-            int returned = Arrays.binarySearch(array, i + 1, array.length, toFind);
-            if (returned > 0)
-            {
+            int returned = BinarySearch.binarySearch(array, i + 1, array.length - 1, toFind);
+            if (returned > 0) {
                 System.out.println("values that make the given sum :  " + array[i] + " , " + array[returned]);
             }
         }
