c5ef3d61ea2ba296a9327b2dc5abf6859be52ab7
Suresh <mail@sureshsajja.com>
2014-11-02 02:16:21 +0530

    Majority element

--- src/com/coderevisited/arrays/MajorityElement.java
+++ src/com/coderevisited/arrays/MajorityElement.java
@@ -1,5 +1,13 @@
-package com.coderevisited.coding.arrays;
+package com.coderevisited.arrays;
 
+/**
+ * Majority element is defined as element that appears more than N/2 times
+ * 1. assume element at 0 is majority and say it's count as 1
+ * 2. for each element i from 1 to N - 1
+ * 3. if 'i' is majority element increment count, else decrement count
+ * 4. if count is '0', make majority element as current element and count as 1
+ * 5. Once we find majority element from the above loop, we have to find if it is actually majority by traversing array one time.
+ */
 public class MajorityElement
 {
     public static void main(String[] args)
@@ -41,7 +49,5 @@ public class MajorityElement
             return index;
         else
             return -1;
-
-
     }
 }
