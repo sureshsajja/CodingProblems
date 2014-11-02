bb77eeee78c51ae54061381957f0bb2e4c9a55ef
Suresh <mail@sureshsajja.com>
2014-11-02 10:48:35 +0530

    Largest sub array with contiguous elements

---
+++ src/com/coderevisited/arrays/LargestSubArrayContiguous.java
@@ -0,0 +1,46 @@
+package com.coderevisited.arrays;
+
+/**
+ * Run two loops, i start from 0 to n-2, j start from i+1 to n-1
+ * Assume sub array starts at i and ends at j.
+ * for each iteration, update min and max in the sub array
+ * If max - min == j - i, that is the sub array with contiguous elements. Update maxLength
+ */
+public class LargestSubArrayContiguous
+{
+    public static void main(String[] args)
+    {
+        int[] array = new int[]{1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
+        int length = findMaxLengthSubArray(array);
+        System.out.println("Size Largest sub Array With contiguous elements " + length);
+    }
+
+    private static int findMaxLengthSubArray(int[] array)
+    {
+
+        int maxLength = 1;
+
+        for (int i = 0; i < array.length - 1; i++) {
+
+            int min = array[i];
+            int max = array[i];
+
+            for (int j = i + 1; j < array.length; j++) {
+
+                if (min > array[j]) {
+                    min = array[j];
+                }
+                if (max < array[j]) {
+                    max = array[j];
+                }
+
+                if (max - min == j - i) {
+                    if (j - i > maxLength)
+                        maxLength = j - i + 1;
+                }
+            }
+        }
+
+        return maxLength;
+    }
+}
