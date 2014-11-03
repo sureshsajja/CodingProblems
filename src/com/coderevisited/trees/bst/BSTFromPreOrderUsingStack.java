bd9c55bee5e64e57c5d4b13edd408594ae2ce775
Suresh Sajja <suresh.sajja@autodesk.com>
2014-11-03 19:03:58 +0530

    BST from preOrder

---
+++ src/com/coderevisited/trees/bst/BSTFromPreOrderUsingStack.java
@@ -0,0 +1,50 @@
+package com.coderevisited.trees.bst;
+
+
+import java.util.Stack;
+
+import com.coderevisited.trees.binarytree.BinaryTreeNode;
+import com.coderevisited.trees.binarytree.BinaryTreeTraversal;
+
+/**
+ * 1. Push first node as root to stack 2. for each int  i from pre-order, pop from stack until we found top element is
+ * greater than  i 3. if last pop element is not null, set i as right child 4. if last pop is null, set i as left child
+ * to the peek. 5. add newly constructed node to stack
+ */
+public class BSTFromPreOrderUsingStack
+{
+    public static void main(String[] args)
+    {
+        int[] preOrder = new int[]{10, 5, 1, 7, 40, 50};
+        BinaryTreeNode root = constructBST(preOrder);
+        BinaryTreeTraversal.printInOrder(root);
+    }
+
+    private static BinaryTreeNode constructBST(int[] preOrder)
+    {
+        Stack<BinaryTreeNode> stack = new Stack<BinaryTreeNode>();
+        BinaryTreeNode root = null;
+        if (preOrder.length > 1) {
+            root = new BinaryTreeNode(preOrder[0], null, null);
+            stack.push(root);
+        }
+        for (int i = 1; i < preOrder.length; i++) {
+
+            BinaryTreeNode peek = null;
+            while (!stack.isEmpty() && preOrder[i] > stack.peek().getValue()) {
+                peek = stack.pop();
+            }
+            BinaryTreeNode node = new BinaryTreeNode(preOrder[i], null, null);
+
+            if (peek != null) {
+                peek.setRight(node);
+            }
+            if (peek == null) {
+                peek = stack.peek();
+                peek.setLeft(node);
+            }
+            stack.push(node);
+        }
+        return root;
+    }
+}
