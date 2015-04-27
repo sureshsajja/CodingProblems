8d6f9a51210a4de8d52b0ddc57d0006833dfc71f
Suresh Sajja <mail@sureshsajja.com>
2015-04-27 22:28:08 +0530

    Code-Bee

---
+++ src/com/hackerearth/codebee/FindingDinosaurs.java
@@ -0,0 +1,127 @@
+package com.hackerearth.codebee;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/**
+ * User : Suresh
+ * Date : 25/04/15
+ */
+
+/**
+ * Ross and Rachel are on a date. Ross, being the super geek he is, takes Rachel to see dinosaur exhibits.
+ * Each dinosaur has a name and K attributes which are described by an ordered K-tuple ( A1, A2, A3,..., AK ). Each attribute Ai is an integer between 0 to L inclusive. Ross tells Rachel about N dinosaurs. For each dinosaur, he tells her its name and gives her the K-tuple describing its attributes.
+ * Afterwards, he wants to play a quiz with Rachel. He gives Rachel Q K-tuples and ask her which dinosaur they belongs to. As Rachel loves Ross and doesn't want him to think she wasn't paying attention, she wants to answer all questions correctly. Help her in this task.
+ * <p/>
+ * Input:
+ * The first line contains N, K, L and Q, the number of dinosaurs, the size of the K-tuple, the maximum value of any attribute and the number of questions that Ross asks.
+ * N lines follow. The next ith line contains a string which is the name of the ith dinosaur and K space separated integer which are the attributes for the ith dinosaur.
+ * Q lines follow. The next ith line contains K space separated integer denoting a K-tuple.
+ * <p/>
+ * Output:
+ * For each question, output the name of the dinosaur that the K-tuple belongs to.
+ * It is also possible that Ross is asking a trick question and the tuple doesn't describe any dinosaur. In such a case, output "You cant fool me :P" (Quotes are for clarity)
+ * <p/>
+ * Constraints:
+ * 1<=N<=100000
+ * 1<=K<=5
+ * 1<=L<=15
+ * 1<=Q<=100000
+ * 0<=Ai<=L
+ * 1<=Length of Dinosaur's name<=10
+ * Dinosaur's name will contain only lowercase alphabets [a-z].
+ * <p/>
+ * Note:
+ * No two dinosaurs will have the same K-tuple or name.
+ * <p/>
+ * Sample Input
+ * ============
+ * 5 3 9 5
+ * baryonyx 5 1 7
+ * jobaria 1 2 3
+ * oviraptor 9 7 1
+ * troodon 6 9 5
+ * minmi 7 4 5
+ * 9 7 1
+ * 1 2 3
+ * 5 8 8
+ * 1 2 3
+ * 7 4 5
+ * <p/>
+ * <p/>
+ * Sample Output
+ * =============
+ * oviraptor
+ * jobaria
+ * You cant fool me :P
+ * jobaria
+ * minmi
+ * <p/>
+ * Algorithm:
+ * =========
+ * This is just a demonstration of Hashcode implementation
+ * http://stackoverflow.com/questions/113511/hash-code-implementation
+ */
+public class FindingDinosaurs {
+
+    private static BufferedReader reader;
+    private static StringTokenizer tokenizer;
+
+    private static String next() throws IOException {
+        while (!tokenizer.hasMoreTokens()) {
+            tokenizer = new StringTokenizer(
+                    reader.readLine());
+        }
+        return tokenizer.nextToken();
+    }
+
+    private static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        tokenizer = new StringTokenizer("");
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+
+        int N = nextInt();
+        int K = nextInt();
+        int L = nextInt();
+        int Q = nextInt();
+
+
+        Map<Integer, String> dinosaurs = new HashMap<>();
+        for (int n = 0; n < N; n++) {
+            String value = next();
+
+            int key = 1;
+            for (int k = 0; k < K; k++) {
+                int a = nextInt();
+                key = 31 * key + a;
+            }
+            dinosaurs.put(key, value);
+        }
+
+        for (int q = 0; q < Q; q++) {
+            int key = 1;
+            for (int k = 0; k < K; k++) {
+                int a = nextInt();
+                key = 31 * key + a;
+            }
+            if (dinosaurs.get(key) != null) {
+                pw.println(dinosaurs.get(key));
+            } else {
+                pw.println("You cant fool me :P");
+            }
+        }
+
+        reader.close();
+        pw.close();
+    }
+
+
+}
---
+++ src/com/hackerearth/codebee/HeadChefMonica.java
@@ -0,0 +1,140 @@
+package com.hackerearth.codebee;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * User : Suresh
+ * Date : 25/04/15
+ */
+
+/**
+ * Monica is the Head-chef at Javu's restaurant. Javu has N kitchen workers, numbered from 1 to N. Each kitchen worker has expertise in exactly one of the following fields:
+ * <p/>
+ * VEG - denoted as V
+ * NONVEG - denoted as N
+ * GRILL - denoted as G
+ * TOAST - denoted as T
+ * SANDWICH - denoted as S
+ * BURGER - denoted as B
+ * PIZZA - denoted as P
+ * You'll be given the string S1 of length N, where ith character defines the field of expertise of ith worker, 1 ≤ i ≤ N.
+ * Now, a customer's order contains 3 fields:
+ * <p/>
+ * 1st field	2nd field	3rd field
+ * VEG or NONVEG (V or N)	GRILL or TOAST (G or T)	SANDWICH or BURGER or PIZZA (S or B or P)
+ * You'll be given a string S2 of length 3, where jth character defines the jth field of the order, 1 ≤ j ≤ 3.
+ * The first character will either be V or N. Second character will either be G or T. Third character will either be S or B or P.
+ * <p/>
+ * According to the customer order, Monica has to form a team of workers who can prepare the order. Javu has received M orders at the moment. You have to find in how many ways can Monica prepare a team of 3 workers for each of the orders.
+ * <p/>
+ * Note: One worker can only work in one team. See the sample test case for clarification.
+ * <p/>
+ * Input:
+ * First line of input contains N - number of workers.
+ * Second line contains a string S1 of length N, where ith character defines the field of expertise of ith worker, 1 ≤ i ≤ N.
+ * Third lines contains M - number of orders.
+ * Each of the next M lines contain an order query S2.
+ * <p/>
+ * Output:
+ * For each order query, print the number of ways Monica can form a team of 3 workers who can prepare the order.
+ * Since the answer can be very large, print it modulo 1000000007 ( 109 + 7 )
+ * <p/>
+ * Constraints:
+ * 1 ≤ N ≤ 1000000
+ * String S1 will be of length N and will only contain upper case character [V, N, G, T, S, B, P]
+ * 1 ≤ M ≤ 300000
+ * String S2 will be of length 3 and will only contain upper case character [V, N, G, T, S, B, P]
+ * <p/>
+ * Sample Input
+ * ============
+ * 11
+ * VBVNSSTSBGG
+ * 5
+ * VGS
+ * NTS
+ * NGS
+ * VGS
+ * VGB
+ * Sample Output
+ * =============
+ * 12
+ * 2
+ * 0
+ * 1
+ * 0
+ * Explanation
+ * <p>Initially we have following number of employees of each field:
+ * {'B': 2, 'G': 2, 'N': 1, 'P': 0, 'S': 3, 'T': 1, 'V': 2}</p>
+ * <p>For order VGS, Monica can make a team in 2<em>2</em>3 = 12 ways.
+ * After 1st order we decrement 1 employee each from V, G and ; so available number of employees are:
+ * {'B': 2, 'G': 1, 'N': 1, 'P': 0, 'S': 2, 'T': 1, 'V': 1}</p>
+ * <p>For order NTS, there are 1<em>1</em>2 = 2 ways.
+ * After 2nd order, available employees are:
+ * {'B': 2, 'G': 1, 'N': 0, 'P': 0, 'S': 1, 'T': 0, 'V': 1}</p>
+ * <p>For order NGS, since number of employees for 'N' is 0, we cannot prepare a team even though the other employees for 'G' and 'S'. So the answer is zero. Since the team is not formed we don't decrement the number for 'G' and 'S'.
+ * After 3rd order, available employees are again:
+ * {'B': 2, 'G': 1, 'N': 0, 'P': 0, 'S': 1, 'T': 0, 'V': 1}</p>
+ * <p>For order VGS, there is 1<em>1</em>1 = 1 way.
+ * Similarly we can find for order VGB as well. </p>
+ *
+ * Algorithms
+ * =========
+ * 1. Construct a histogram from given S1
+ * 2. For each S2, check if chefs can be selected.
+ */
+public class HeadChefMonica {
+
+
+    private static BufferedReader reader;
+    private static StringTokenizer tokenizer;
+
+    private static String next() throws IOException {
+        while (!tokenizer.hasMoreTokens()) {
+            tokenizer = new StringTokenizer(
+                    reader.readLine());
+        }
+        return tokenizer.nextToken();
+    }
+
+    private static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        tokenizer = new StringTokenizer("");
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+
+        int N = nextInt();
+        char[] sArray = next().toCharArray();
+        int[] hist = new int[26];
+        for (char c : sArray) {
+            hist[c - 'A']++;
+        }
+
+        int M = nextInt();
+
+
+        for (int i = 0; i < M; i++) {
+            char[] s2Array = next().toCharArray();
+            long answer = 1L;
+            if (hist[s2Array[0] - 'A'] != 0 && hist[s2Array[1] - 'A'] != 0 && hist[s2Array[2] - 'A'] != 0) {
+                answer = answer * hist[s2Array[0] - 'A'] * hist[s2Array[1] - 'A'] * hist[s2Array[2] - 'A'];
+                hist[s2Array[0] - 'A']--;
+                hist[s2Array[1] - 'A']--;
+                hist[s2Array[2] - 'A']--;
+                pw.println(answer % 1000000007);
+            } else {
+                pw.println(0);
+            }
+        }
+
+        reader.close();
+        pw.close();
+    }
+
+
+}
