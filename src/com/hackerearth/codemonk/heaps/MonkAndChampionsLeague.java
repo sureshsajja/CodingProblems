011acfaf090e640acc489667b2a0040e10b7dd4a
Suresh <mail@sureshsajja.com>
2015-07-27 21:43:48 +0530

    Code Monk Heap

---
+++ src/com/hackerearth/codemonk/heaps/MonkAndChampionsLeague.java
@@ -0,0 +1,92 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Suresh Sajja
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package com.hackerearth.codemonk.heaps;
+
+import java.io.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+/**
+ * User :  Suresh
+ * Date :  27/07/15
+ * Version : v1
+ */
+public class MonkAndChampionsLeague {
+
+
+    private static BufferedReader reader;
+    private static StringTokenizer tokenizer;
+
+    private static String next() throws IOException {
+        while (!tokenizer.hasMoreTokens()) {
+            tokenizer = new StringTokenizer(
+                    reader.readLine());
+        }
+        return tokenizer.nextToken();
+    }
+
+    private static int nextInt() throws IOException {
+        return parseInt(next());
+    }
+
+    public static void main(String[] args) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(in));
+        tokenizer = new StringTokenizer("");
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(out)));
+
+
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(100000, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer i, Integer j) {
+                return i < (int) j ? 1 : (i == (int) j ? 0 : -1);
+            }
+        });
+
+        int M = nextInt();
+        int N = nextInt();
+        for (int m = 0; m < M; m++) {
+            queue.add(nextInt());
+        }
+
+        int sum = 0;
+        for (int n = 0; n < N; n++) {
+            int i = queue.poll();
+            sum += i;
+            queue.add(i - 1);
+        }
+
+        pw.println(sum);
+
+
+        reader.close();
+        pw.close();
+    }
+}
