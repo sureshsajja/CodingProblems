1ebb2c43cc2a7b2304b9aaeec66f10758839b7fe
Suresh Sajja <Suresh@Sureshs-MacBook-Pro.local>
2015-11-16 19:13:30 +0530

    Added maven support

---
+++ src/test/java/com/coderevisited/SampleTest.java
@@ -0,0 +1,19 @@
+package com.coderevisited;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * User    : Suresh
+ * Date    : 16/11/15
+ * Version : v1
+ */
+public class SampleTest {
+
+
+    @Test
+    public void testMe() {
+        Assert.assertEquals("1", "1");
+    }
+
+}
