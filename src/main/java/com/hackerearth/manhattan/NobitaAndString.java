9523ac08ee67284bd6b37d059d52a3c3c39ace63
Suresh <mail@sureshsajja.com>
2015-11-24 23:58:40 +0530

    easy

---
+++ src/main/java/com/hackerearth/manhattan/NobitaAndString.java
@@ -0,0 +1,73 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 CodeRevisited.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package com.hackerearth.manhattan;
+
+import java.io.*;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+/**
+ * User    : Suresh
+ * Date    : 24/11/15
+ * Version : v1
+ * https://www.hackerearth.com/problem/algorithm/nobita-and-string-4/
+ */
+public class NobitaAndString {
+
+
+    private static BufferedReader reader;
+
+    private static String next() throws IOException {
+        return reader.readLine();
+    }
+
+    private static int nextInt() throws IOException {
+        return parseInt(next());
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        reader = new BufferedReader(new InputStreamReader(in));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(out)));
+
+        int T = nextInt();
+        for (int t = 0; t < T; t++) {
+            String[] array = next().split(" ");
+            for (int i = array.length - 1; i >= 0; i--) {
+                pw.print(array[i] + " ");
+            }
+            pw.println();
+        }
+
+        reader.close();
+        pw.close();
+
+    }
+
+
+}
---
+++ src/main/java/com/hackerearth/practo/SavePatients.java
@@ -0,0 +1,99 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 CodeRevisited.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package com.hackerearth.practo;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+/**
+ * User    : Suresh
+ * Date    : 24/11/15
+ * Version : v1
+ * https://www.hackerearth.com/problem/algorithm/save-patients/
+ */
+public class SavePatients {
+
+    private static BufferedReader reader;
+    private static StringTokenizer tokenizer;
+
+    private static String next() throws IOException {
+        while (!tokenizer.hasMoreTokens()) {
+            tokenizer = new StringTokenizer(
+                    reader.readLine());
+        }
+        return tokenizer.nextToken();
+    }
+
+    private static int nextInt() throws IOException {
+        return parseInt(next());
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        reader = new BufferedReader(new InputStreamReader(in));
+        tokenizer = new StringTokenizer("");
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(out)));
+
+
+        int N = nextInt();
+        int[] vaccine = new int[N];
+        int[] patients = new int[N];
+        for (int i = 0; i < N; i++) {
+            vaccine[i] = nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            patients[i] = nextInt();
+        }
+
+        Arrays.sort(vaccine);
+        Arrays.sort(patients);
+
+        boolean canSave = true;
+        for (int i = 0; i < N; i++) {
+            if (patients[i] > vaccine[i]) {
+                canSave = false;
+                break;
+            }
+        }
+
+        if (canSave) {
+            pw.println("Yes");
+        } else {
+            pw.println("No");
+        }
+
+        reader.close();
+        pw.close();
+
+    }
+}
