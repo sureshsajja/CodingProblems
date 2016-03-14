6290b67b27b0ce3db066d6d1d77e99522e6c11df
Suresh <mail@sureshsajja.com>
2016-03-14 06:49:04 +0530

    TimeConversion

---
+++ src/main/java/com/hackerrank/warmup/TimeConversion.java
@@ -0,0 +1,90 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 CodeRevisited.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.hackerrank.warmup;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+/**
+ * User    : Suresh
+ * Date    : 13/03/16
+ * Version : v1
+ */
+
+/**
+ * https://www.hackerrank.com/challenges/time-conversion
+ */
+public class TimeConversion {
+
+    private static BufferedReader reader;
+    private static StringTokenizer tokenizer;
+
+    private static String next() throws IOException {
+        while (!tokenizer.hasMoreTokens()) {
+            tokenizer = new StringTokenizer(
+                    reader.readLine());
+        }
+        return tokenizer.nextToken();
+    }
+
+    private static int nextInt() throws IOException {
+        return parseInt(next());
+    }
+
+    private static long nextLong() throws IOException {
+        return parseLong(next());
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        reader = new BufferedReader(new InputStreamReader(in));
+        tokenizer = new StringTokenizer("");
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(out)));
+
+        String time = next();
+        String hour = time.substring(0, 2);
+
+        if(time.contains("PM") && !hour.equals("12")){
+            int hr = Integer.parseInt(hour) + 12;
+            time = time.replace(hour, String.valueOf(hr));
+        }else if(time.contains("AM") && hour.equals("12")){
+            time = time.replace(hour, "00");
+        }
+
+        time = time.replace("AM", "");
+        time = time.replace("PM", "");
+
+        pw.println(time);
+
+        reader.close();
+        pw.close();
+
+    }
+}
